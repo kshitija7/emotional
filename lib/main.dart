b133561f23ac5b2efadea4e2c6ba9790f626687d
kshitija777 <kshitijashekhar@gmail.com>
2020-06-21 21:13:01 +0530

    local changes

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
+import 'package:relax/slider.dart';
+
+
+void main() => runApp(Emotions());
+
+
+class Emotions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Emotional Hygiene",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.blueAccent,
+
+      ),
+      home:Cards(),
+    );
+  }
+}
+
+class Cards extends StatefulWidget {
+  @override
+  _CardsState createState() => _CardsState();
+}
+
+class _CardsState extends State<Cards> with
+TickerProviderStateMixin{
+   
+
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Emotional Hygiene')),
+      ),
+      body: Center(
+        
+        
+        child: new Column(children: <Widget>[
+
+        
+        Container(
+          
+          
+
+          height:MediaQuery.of(context).size.height*0.7,
+          child:TinderSwapCard (
+            orientation:AmassOrientation.TOP,
+            totalNum:5,
+            stackNum: 4,
+            maxWidth: MediaQuery.of(context).size.width*0.7,
+            maxHeight:MediaQuery.of(context).size.width*0.7,
+            minWidth: MediaQuery.of(context).size.width*0.6,
+            minHeight:MediaQuery.of(context).size.width*0.6,
+            
+            
+            cardBuilder: (context,index)=>Column(
+              children: <Widget>[
+                Card(
+                  
+                  
+                  child:Padding(
+                    padding: EdgeInsets.all(10.0),
+                    
+                    child:  Emotion(
+                      
+                    
+                  ) ,
+                  
+                  
+
+                     
+                  )),
+              ],
+            )  
+               
+          
+            )
+        ),
+        ],)
+
+      ),
+      );
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/slider.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+
+class Emotion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                  
+                
+                    child: SliderContainer(
+                     
+                    ),
+                  ),
+            ],
+          ),
+        ));
+  }
+} 
+
+class SliderContainer extends StatefulWidget {
+  @override
+  _SliderContainerState createState() => _SliderContainerState();
+}
+
+class _SliderContainerState extends State<SliderContainer> {
+  static double _lowervalue = 0.0;
+  static double _uppervalue = 10.0;
+  double brightness = 0.0;
+  List<String> questions=[
+      'Are you overthinking?',
+      'Have you been thinking same thoughts since past few days?',
+      'How happy have you felt lately?',
+      'Are you anxious?',
+      'Are you stressed?'
+  ];
+  var index =0;
+  
+ 
+  
+  RangeValues values = RangeValues(_lowervalue, _uppervalue);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('${questions[index]}'),
+        
+        SliderTheme(
+          data: SliderTheme.of(context).copyWith(
+            trackHeight: 15,
+            overlayColor: Colors.transparent,
+            minThumbSeparation: 100,
+            rangeThumbShape: RoundRangeSliderThumbShape(
+              enabledThumbRadius: 10,
+              disabledThumbRadius: 10,
+              
+            ),
+          ),
+         
+          child: Container(
+            child: Slider(
+              
+                label: brightness.abs().toString(),
+                min: _lowervalue,
+                max: _uppervalue,
+                value: brightness,
+                onChanged: (val) {
+                  print(val);
+                  setState(() {
+                    brightness = val;
+                  });
+                }),
+                height: 200,
+                width: 400,
+          ),
+        ),
+      ],
+    );
+  }
+}
